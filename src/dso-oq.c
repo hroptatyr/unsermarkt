ed78d82d74dec1095c7da2148134a6552c3e9ae6
Sebastian Freundt <hroptatyr@fresse.org>
2010-08-29 12:08:51 +0000

    Summary: fix, move oq specific stuff from htws to dso-oq again Keywords: ws Signed-off-by: Sebastian Freundt <hroptatyr@fresse.org>

--- src/dso-oq.c
+++ src/dso-oq.c
@@ -75,6 +75,171 @@ static umoq_t q = NULL;
 /* our websocket support */
 #include "htws.c"
 
+static int status_updated = 0;
+
+static void
+prstcb(char side, uml_t l)
+{
+	char pri[32];
+
+	ffff_m30_s(pri, l->p);
+	mptr += sprintf(mptr, "<%c p=\"%s\" q=\"%u\"/>", side, pri, l->q);
+	return;
+}
+
+static void
+prstbcb(uml_t l, void *UNUSED(clo))
+{
+	prstcb('b', l);
+	return;
+}
+
+static void
+prstacb(uml_t l, void *UNUSED(clo))
+{
+	prstcb('a', l);
+	return;
+}
+
+static void
+pr_otag(void)
+{
+	static const char tag[] = "<quotes>";
+	append(tag, sizeof(tag) - 1);
+	return;
+}
+
+static void
+pr_ctag(void)
+{
+	static const char tag[] = "</quotes>\n";
+	append(tag, sizeof(tag) - 1);
+	return;
+}
+
+static void
+prbdry(void)
+{
+	static const char bdry[] = "--umbdry\r\n";
+	append(bdry, sizeof(bdry) - 1);
+	return;
+}
+
+static void
+prcty(void)
+{
+	static const char cty[] = "Content-Type: application/xml\n\n\
+<?xml version='1.0'?>\n";
+	append(cty, sizeof(cty) - 1);
+	return;
+}
+
+static void
+prxmlhdr(void)
+{
+	static const char hdr[] = "<?xml version=\"1.0\"?>\n";
+	append(hdr, sizeof(hdr) - 1);
+	return;
+}
+
+static void __attribute__((unused))
+prep_http_status(void)
+{
+	/* http mode */
+	reset();
+	prbdry();
+	prcty();
+	pr_otag();
+	/* go through all bids, then all asks */
+	oq_trav_bids(q, prstbcb, NULL);
+	oq_trav_asks(q, prstacb, NULL);
+	pr_ctag();
+	prbdry();
+	return;
+}
+
+static void
+prep_htws_status(void)
+{
+	/* htws mode */
+	reset();
+	*mptr++ = 0x00;
+	prxmlhdr();
+	pr_otag();
+	/* go through all bids, then all asks */
+	oq_trav_bids(q, prstbcb, NULL);
+	oq_trav_asks(q, prstacb, NULL);
+	pr_ctag();
+	*mptr++ = 0xff;
+	return;
+}
+
+#if 0
+static void
+prhtwshdr(int fd)
+{
+	static const char httphdr[] = "\
+HTTP/1.1 101 WebSocket Protocol Handshake\r\n\
+Date: Tue, 24 Aug 2010 21:51:08 GMT\r\n\
+Server: unsermarkt/0.1\r\n\
+Upgrade: WebSocket\r\n\
+Connection: Upgrade\r\n\
+Sec-WebSocket-Origin: http://www.unserding.org:12768\r\n\
+Sec-WebSocket-Location: ws://www.unserding.org:12768\r\n\
+Transfer-Encoding: chunked\r\n\
+Connection: Keep-Alive\r\n\
+Content-Type: multipart/x-mixed-replace;boundary=\"umbdry\"\r\n\r\n";
+	write(fd, httphdr, sizeof(httphdr) - 1);
+	return;
+}
+#endif
+
+static void
+prstatus(int fd)
+{
+/* prints the current order queue to FD */
+	/* check if status needs updating */
+	if (!status_updated) {
+		prep_htws_status();
+		status_updated = 1;
+	}
+
+#if 0
+/* only in http mode */
+	{
+		char len[16];
+		size_t lenlen;
+		size_t chlen = mptr - mbuf;
+
+		/* compute the chunk length */
+		lenlen = snprintf(len, sizeof(len), "%zx\r\n", chlen);
+		write(fd, len, lenlen);
+		/* put the final \r\n */
+		*mptr++ = '\r';
+		*mptr++ = '\n';
+	}
+#else
+/* htws mode */
+	;
+#endif
+	write(fd, mbuf, mptr - mbuf);
+	return;
+}
+
+static void
+upstatus(void)
+{
+/* for all fds in the htpush queue print the status */
+	/* flag status as outdated */
+	status_updated = 0;
+	for (int i = 0; i < MAX_CLIENTS; i++) {
+		if (htpush[i] > 0) {
+			prstatus(htpush[i]);
+		}
+	}
+	return;
+}
+
 
 /**
  * Take the stuff in MSG of size MSGLEN coming from FD and process it.
@@ -84,6 +249,9 @@ handle_data(int fd, char *msg, size_t msglen)
 {
 	if (htws_get_p(msg, msglen)) {
 		UM_DEBUG(MOD_PRE ": htws push request\n");
+		/* first of all render the status bit void
+		 * coz the htws handler uses the same buffer */
+		status_updated = 0;
 		if (htws_handle_get(fd, msg, msglen) < 0) {
 			return -1;
 		}

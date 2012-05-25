04b1b9799c99c8d971249171523c6b960aac6a1c
Sebastian Freundt <freundt@ga-group.nl>
2012-05-25 15:48:48 +0000

    minor, actually build a LOB and print it
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/um-quomon.c
+++ src/um-quomon.c
@@ -42,6 +42,7 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <time.h>
+#include <assert.h>
 
 #if defined HAVE_SYS_SOCKET_H
 # include <sys/socket.h>
@@ -73,19 +74,234 @@
 #include <m30.h>
 #include <m62.h>
 
+/* for the limit order book */
+#include <sys/mman.h>
+
 #include <ncurses.h>
 
-static int nr, nc;
+typedef uint32_t lobidx_t;
+typedef struct lob_cli_s *lob_cli_t;
+typedef union lob_side_u *lob_side_t;
+
+/* the client */
+struct lob_cli_s {
+	ud_sockaddr_u sa;
+	lobidx_t b;
+	lobidx_t a;
+};
+
+/* our limit order book, well just level 2 */
+struct lob_entry_s {
+	/* client, that is addr+port */
+	lob_cli_t cli;
+	/* price */
+	m30_t p;
+	/* quantity */
+	m30_t q;
+};
+
+/* same but with navigation */
+struct lob_entnav_s {
+	struct lob_entry_s v;
+	lobidx_t prev;
+	lobidx_t next;
+};
+
+/* all entries of one side */
+union lob_side_u {
+	struct lob_entnav_s e[];
+
+	struct {
+		lobidx_t head;
+		lobidx_t free;
+		/* padding */
+		lobidx_t v[4];
+	};
+};
+
+
+static lob_side_t lobb = NULL;
+static lob_side_t loba = NULL;
+static lob_cli_t cli = NULL;
+static size_t ncli = 0;
+
+#define EAT(y, x)	y->e[x]
+#define NEXT(y, x)	EAT(y, x).next
+#define PREV(y, x)	EAT(y, x).prev
+
+#if defined MAP_ANON && !defined MAP_ANONYMOUS
+# define MAP_ANONYMOUS	MAP_ANON
+#endif	/* MAP_ANON && !MAP_ANONYMOUS */
+#define MAP_MEM		(MAP_PRIVATE | MAP_ANONYMOUS)
+#define PROT_MEM	(PROT_READ | PROT_WRITE)
+
+static void
+init_lob(void)
+{
+	lobb = mmap(NULL, 4096, PROT_MEM, MAP_MEM, -1, 0);
+	loba = mmap(NULL, 4096, PROT_MEM, MAP_MEM, -1, 0);
+	cli = mmap(NULL, 4096, PROT_MEM, MAP_MEM, -1, 0);
+
+	/* init the lob */
+	lobb->free = 1;
+	loba->free = 1;
+	for (lobidx_t i = 1; i < 4096 / sizeof(*lobb->e) - 1; i++) {
+		NEXT(lobb, i) = i + 1;
+		NEXT(loba, i) = i + 1;
+	}
+	return;
+}
+
+static void
+free_lob(void)
+{
+	munmap(lobb, 4096);
+	munmap(loba, 4096);
+	munmap(cli, 4096);
+	return;
+}
+
+static lobidx_t
+lob_ins_at(lob_side_t s, lobidx_t pr, struct lob_entry_s v)
+{
+	lobidx_t nu;
+
+	nu = s->free;
+	assert(nu);
+	assert(nu != pr);
+	s->free = NEXT(s, s->free);
+
+	/* populate the cell */
+	EAT(s, nu).v = v;
+	/* update navigators */
+	if (pr) {
+		lobidx_t nx = NEXT(s, pr);
+
+		NEXT(s, nu) = nx;
+		PREV(s, nu) = pr;
+		NEXT(s, pr) = nu;
+		if (nx) {
+			PREV(s, nx) = nu;
+		}
+	} else {
+		/* ins at head */
+		PREV(s, nu) = 0;
+		if ((NEXT(s, nu) = s->head)) {
+			PREV(s, s->head) = nu;
+		}
+		s->head = nu;
+	}
+
+	assert(pr != nu);
+	return nu;
+}
+
+static void
+lob_rem_at(lob_side_t s, lobidx_t idx)
+{
+	assert(idx);
+
+	/* fix up navigators */
+	if (PREV(s, idx) && NEXT(s, idx)) {
+		PREV(s, NEXT(s, idx)) = PREV(s, idx);
+		NEXT(s, PREV(s, idx)) = NEXT(s, idx);
+	} else if (PREV(s, idx)) {
+		/* tail fiddling */
+		NEXT(s, PREV(s, idx)) = 0;
+	} else {
+		/* head fiddling */
+		if (s->head) {
+			PREV(s, s->head) = 0;
+		}
+		s->head = NEXT(s, idx);
+	}
+
+	assert(idx != s->free);
+	NEXT(s, idx) = s->free;
+	s->free = idx;
+	return;
+}
+
+static lobidx_t
+find_bid(m30_t p)
+{
+	lobidx_t idx = 0;
+
+	for (lobidx_t i = lobb->head; i; idx = i, i = NEXT(lobb, i)) {
+		if (EAT(lobb, i).v.p.v < p.v) {
+			return idx;
+		} else if (EAT(lobb, i).v.p.v == p.v) {
+			return i;
+		}
+	}
+	return idx;
+}
+
+static lobidx_t
+find_ask(m30_t p)
+{
+	lobidx_t idx = 0;
+
+	for (lobidx_t i = loba->head; i; idx = i, i = NEXT(loba, i)) {
+		if (EAT(loba, i).v.p.v > p.v) {
+			return idx;
+		} else if (EAT(loba, i).v.p.v == p.v) {
+			return i;
+		}
+	}
+	return idx;
+}
+
+static lob_cli_t
+find_cli(ud_sockaddr_t sa)
+{
+	for (size_t i = 0; i < ncli; i++) {
+		if (memcmp(&cli[i].sa, sa, sizeof(*sa)) == 0) {
+			return cli + i;
+		}
+	}
+	return NULL;
+}
+
+static lob_cli_t
+add_cli(ud_sockaddr_t sa)
+{
+	size_t idx = ncli++;
+
+	cli[idx].sa = *sa;
+	cli[idx].b = 0;
+	cli[idx].a = 0;
+	return cli + idx;
+}
 
 
 /* the actual worker function */
+static int changep = 0;
+
 static void
-mon_pkt_cb(job_t j)
+mon_beef_cb(EV_P_ ev_io *w, int UNUSED(revents))
 {
-	char buf[8192], *epi = buf;
+	ssize_t nread;
+	/* a job */
+	struct job_s j[1];
+	socklen_t lsa = sizeof(j->sa);
+
+	j->sock = w->fd;
+	nread = recvfrom(w->fd, j->buf, sizeof(j->buf), 0, &j->sa.sa, &lsa);
+
+	/* handle the reading */
+	if (UNLIKELY(nread < 0)) {
+		goto out_revok;
+	} else if (nread == 0) {
+		/* no need to bother */
+		goto out_revok;
+	}
+
+	j->blen = nread;
 
 	/* obtain the address in human readable form */
 	{
+		char buf[256], *epi = buf;
 		int fam = ud_sockaddr_fam(&j->sa);
 		const struct sockaddr *sa = ud_sockaddr_addr(&j->sa);
 		uint16_t port = ud_sockaddr_port(&j->sa);
@@ -104,19 +320,54 @@ mon_pkt_cb(job_t j)
 	case 0x7575: {
 		char *pbuf = UDPC_PAYLOAD(JOB_PACKET(j).pbuf);
 		size_t plen = UDPC_PAYLLEN(JOB_PACKET(j).plen);
+		lob_cli_t c;
+
+
+		if ((c = find_cli(&j->sa)) == NULL) {
+			c = add_cli(&j->sa);
+		}
 
 		for (scom_t sp = (void*)pbuf, ep = (void*)(pbuf + plen);
 		     sp < ep;
 		     sp += scom_tick_size(sp) *
 			     (sizeof(struct sndwch_s) / sizeof(*sp))) {
 			uint16_t ttf = scom_thdr_ttf(sp);
+			const_sl1t_t l1t;
+			struct lob_entry_s v;
+
+			l1t = (const void*)sp;
+			/* populate the value tables */
+			v.cli = c;
+			v.p = (m30_t)l1t->v[0];
+			v.q = (m30_t)l1t->v[1];
 
 			switch (ttf) {
+				lobidx_t e;
 			case SL1T_TTF_BID:
-				mvprintw(nr / 2, nc / 2 - 1, "b");
+				/* delete the former bid first */
+				if (c->b) {
+					lob_rem_at(lobb, c->b);
+				}
+				/* find our spot in the lob */
+				e = find_bid(v.p);
+				/* and insert */
+				e = lob_ins_at(lobb, e, v);
+				/* update client info */
+				c->b = e;
+				changep = 1;
 				break;
 			case SL1T_TTF_ASK:
-				mvprintw(nr / 2, nc / 2 + 1, "a");
+				/* delete the former ask first */
+				if (c->a) {
+					lob_rem_at(loba, c->a);
+				}
+				/* find our spot in the lob */
+				e = find_ask(v.p);
+				/* and insert */
+				e = lob_ins_at(loba, e, v);
+				/* update client info */
+				c->a = e;
+				changep = 1;
 				break;
 			case SL1T_TTF_TRA:
 			case SL1T_TTF_FIX:
@@ -131,7 +382,21 @@ mon_pkt_cb(job_t j)
 				/* snaps */
 			case SSNP_FLAVOUR:
 			case SBAP_FLAVOUR:
-				mvprintw(nr / 2 + 1, nc / 2 - 1, "S S");
+				if (c->b) {
+					lob_rem_at(lobb, c->b);
+				}
+				e = find_bid(v.p);
+				e = lob_ins_at(lobb, e, v);
+				c->b = e;
+
+				if (c->a) {
+					lob_rem_at(loba, c->a);
+				}
+				v.p = v.q;
+				e = find_ask(v.q);
+				e = lob_ins_at(loba, e, v);
+				c->a = e;
+				changep = 1;
 				break;
 
 				/* candles */
@@ -141,7 +406,6 @@ mon_pkt_cb(job_t j)
 			case SL1T_TTF_FIX | SCOM_FLAG_LM:
 			case SL1T_TTF_STL | SCOM_FLAG_LM:
 			case SL1T_TTF_AUC | SCOM_FLAG_LM:
-				mvprintw(nr / 2, nc / 2 - 1, "C");
 				break;
 
 			case SCOM_TTF_UNK:
@@ -159,42 +423,74 @@ mon_pkt_cb(job_t j)
 	default:
 		break;
 	}
-	refresh();
-	return;
-}
 
-static void
-mon_beef_cb(EV_P_ ev_io *w, int UNUSED(revents))
-{
-	ssize_t nread;
-	/* a job */
-	struct job_s j[1];
-	socklen_t lsa = sizeof(j->sa);
-
-	j->sock = w->fd;
-	nread = recvfrom(w->fd, j->buf, JOB_BUF_SIZE, 0, &j->sa.sa, &lsa);
-
-	/* handle the reading */
-	if (UNLIKELY(nread < 0)) {
-		goto out_revok;
-	} else if (nread == 0) {
-		/* no need to bother */
-		goto out_revok;
-	}
-
-	j->blen = nread;
-
-	/* decode the guy */
-	(void)mon_pkt_cb(j);
 out_revok:
 	return;
 }
 
-
-static ev_signal __sigint_watcher;
-static ev_signal __sighup_watcher;
-static ev_signal __sigterm_watcher;
-static ev_signal __sigpipe_watcher;
+static void
+render_cb(EV_P_ ev_timer *w, int UNUSED(revents))
+{
+	unsigned int nr = getmaxy(stdscr);
+	unsigned int nc = getmaxx(stdscr);
+
+	if (!changep) {
+		/* don't bother */
+		return;
+	}
+
+	/* start with a clear window */
+	clear();
+
+	/* go through bids */
+	for (lobidx_t i = lobb->head, j = 1;
+	     i && j < nr;
+	     i = NEXT(lobb, i), j++) {
+		char tmp[64], *p = tmp;
+
+		p += snprintf(p, sizeof(tmp), "%p ", EAT(lobb, i).v.cli);
+		p += ffff_m30_s(p, EAT(lobb, i).v.p);
+		*p = '\0';
+		mvprintw(j, 10, tmp);
+		nc = 10 + 2 + p - tmp;
+	}
+
+	for (lobidx_t i = loba->head, j = 1;
+	     i && j < nr;
+	     i = NEXT(loba, i), j++) {
+		char tmp[64], *p = tmp;
+
+		p += ffff_m30_s(p, EAT(loba, i).v.p);
+		p += snprintf(p, sizeof(tmp), " %p", EAT(loba, i).v.cli);
+		*p = '\0';
+		mvprintw(j, nc, tmp);
+	}
+
+	/* print a note on how to quit */
+	mvprintw(nr - 1, 0, "Press q to quit");
+
+	/* actually render the window */
+	refresh();
+
+	/* reset state */
+	changep = 0;
+
+	/* and then set the timer again */
+	ev_timer_again(EV_A_ w);
+	return;
+}
+
+static void
+keypress_cb(EV_P_ ev_io *UNUSED(w), int UNUSED(revents))
+{
+	switch (getch()) {
+	case 'q':
+		ev_unloop(EV_A_ EVUNLOOP_ALL);
+	default:
+		break;
+	}
+	return;
+}
 
 static void
 sigint_cb(EV_P_ ev_signal *UNUSED(w), int UNUSED(revents))
@@ -239,14 +535,16 @@ main(int argc, char *argv[])
 {
 	/* use the default event loop unless you have special needs */
 	struct ev_loop *loop;
-	ev_signal *sigint_watcher = &__sigint_watcher;
-	ev_signal *sighup_watcher = &__sighup_watcher;
-	ev_signal *sigterm_watcher = &__sigterm_watcher;
-	ev_signal *sigpipe_watcher = &__sigpipe_watcher;
 	ev_io *beef = NULL;
 	size_t nbeef = 0;
 	/* args */
 	struct umqm_args_info argi[1];
+	/* ev goodies */
+	ev_signal sigint_watcher[1];
+	ev_signal sighup_watcher[1];
+	ev_signal sigterm_watcher[1];
+	ev_signal sigpipe_watcher[1];
+	ev_timer render[1];
 
 	/* parse the command line */
 	if (umqm_parser(argc, argv, argi)) {
@@ -268,9 +566,12 @@ main(int argc, char *argv[])
 	/* initialise a SIGHUP handler */
 	ev_signal_init(sighup_watcher, sighup_cb, SIGHUP);
 	ev_signal_start(EV_A_ sighup_watcher);
+	/* initialise a timer */
+	ev_timer_init(render, render_cb, 0.1, 0.1);
+	ev_timer_start(EV_A_ render);
 
 	/* make some room for the control channel and the beef chans */
-	nbeef = argi->beef_given + 1;
+	nbeef = argi->beef_given + 1 + 1;
 	beef = malloc(nbeef * sizeof(*beef));
 
 	/* attach a multicast listener
@@ -294,13 +595,25 @@ main(int argc, char *argv[])
 	initscr();
 	keypad(stdscr, TRUE);
 	noecho();
-	getmaxyx(stdscr, nr, nc);
+
+	/* watch the terminal */
+	{
+		ev_io *keyp = beef + 1 + argi->beef_given;
+		ev_io_init(keyp, keypress_cb, STDOUT_FILENO, EV_READ);
+		ev_io_start(EV_A_ keyp);
+	}
+
+	/* init the limit order book */
+	init_lob();
 
 	/* now wait for events to arrive */
 	ev_loop(EV_A_ 0);
 
 	/* reset the screen */
 	endwin();
+
+	/* finish order book */
+	free_lob();
 
 	/* detaching beef channels */
 	for (unsigned int i = 0; i < nbeef; i++) {
